96e6449275c8a0500303407a77d7c9e4f44c8168
kammagik@gmail.com <kammagik@gmail.com>
2018-01-23 04:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+  lowercase.toLowerCase() === lowercase;
+  var uppercase = 'HELLO!'
+  uppercase.toUpperCase() === upppercase;
+  if lowercase === true
+  return string sayHiToGrandma();
 }
