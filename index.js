e7d9848da7730ba510b45d79eb69b681fac6e677
kammagik@gmail.com <kammagik@gmail.com>
2018-01-23 04:34:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
- if (string.toLowerCase === string)
+ if (string.toLowerCase() === string)
  return "I can't hear you"
 }
+  if (string.toUpperCase()=== string)
+  return ""
