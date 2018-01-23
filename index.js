90e79fb3014ef94cdaf8392b0d0f16e39547a36a
kammagik@gmail.com <kammagik@gmail.com>
2018-01-23 04:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,12 @@ function sayHiToGrandma(string) {
  if (string.toLowerCase() === string) {
  return "I can't hear you";
 }
- else if (string.toUpperCase() === string){
+  if (string.toUpperCase() === string){
   return "YES INDEED!";
   }
-  else return "I love you too";
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
+ 
+   return "Are you eating enough?"
+ }
